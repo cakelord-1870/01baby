509f933fa3f3bf11b1ff9eb9bc4a795e6dfa00c1
cakelord-1870 <4300holdingsinc@gmail.com>
2021-07-13 15:27:45 -0400

    init

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // install express with `npm install express` 
 const express = require('express');
 const app = express();
+const port = 3000; 
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 app.use(express.static('public'));
 
 // export 'app'
+
 module.exports = app;
